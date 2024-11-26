76d87a8d93c2c7df6f57fa30caa3ee1bc7e20516
Hyuk Min <judicious210@gmail.com>
2024-11-26 01:55:58 +0000

    Import all terminal settings from honey_tip_pot

--- terminal_settings/init_my_shell.sh
+++ terminal_settings/init_my_shell.sh
@@ -53,7 +53,7 @@ fi
 # 6. 파일을 홈 디렉토리로 심볼릭 링크 생성
 echo "[6] Creating symbolic links for configuration files in home directory..."
 
-FILES=(.gitconfig .my_aliases.sh .vimrc .zshrc)
+FILES=(.gitconfig .my_aliases.sh .vimrc .zshrc .p10k.zsh)
 for FILE in "${FILES[@]}"; do
     if [ -f "$FILE" ]; then
         if [ -e "$HOME/$FILE" ]; then
@@ -76,4 +76,6 @@ else
     echo "zsh is already the default shell."
 fi
 
-echo "[8] Installation and setup complete. Please restart your terminal or open a new terminal session."
+echo "[8] Installation and setup complete. Restarting your terminal shell as zsh"
+
+zsh
